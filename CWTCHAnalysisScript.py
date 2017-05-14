94ed96e5ecc2142d10154ecc8d0ed38130192b81
mattjonesuk <jonesmt1@cardiff.ac.uk>
2017-05-14 11:05:56 +0100

    FinishedVersion
    Finished version of the scripts.  The only change left to make is in
    marking the correct stimuli in the PAL task (see comment in processData
    function.

--- CWTCHAnalysisScript.py
+++ CWTCHAnalysisScript.py
@@ -1,3 +1,90 @@
-from CWTCHFunctions import loadData
+import sys
+import CWTCHFunctions as cf
+import os
+import pandas as pd
 
-jsonData = loadData("/Users/mattjones/Downloads/data.json")
+
+## These variables need editing before running the script: 
+expectedNoSubjects = 5
+jsonDataPath = "/Users/mattjones/Downloads/data.json"
+outputFolder = "/Users/mattjones/Downloads/output/"
+
+## NOTHING BELOW NEEDS EDITING!!
+# Load data
+jsonData = cf.loadData(jsonDataPath)
+
+# Remove blank entries and check size matches
+jsonData = [x for x in jsonData if x]
+
+if len(jsonData) != expectedNoSubjects:
+    sys.exit("number of loaded subjects does not match the number of expected subjects")
+    
+
+# Create an empty list to store all data for each task
+allOddityData = []
+allSpatialData = []
+allPalData = []
+errors = []
+
+# Create filepaths for CSV files
+oddityOutput = "%sallOddity.csv" % (outputFolder)
+spatialOutput = "%sallSpatial.csv" % (outputFolder)
+palOutput = "%sallPal.csv" % (outputFolder)
+errorOutput = "%serrors.csv" % (outputFolder)
+
+#%% Start loop to analyse each participant
+for index, participantData in enumerate(jsonData):
+    
+    print('Participant:',index)
+    
+    # get task data
+    oddityData = cf.getTaskData(participantData, 'oddity')
+    spatialData = cf.getTaskData(participantData, 'spatial')
+    palData = cf.getTaskData(participantData, 'pal')
+    
+    # Oddity Data
+    if oddityData is 'None':
+        errors.append('No Oddity data for participant - %s' % (participantData[0]['panelId']))
+    else:        
+        # Oddity data processing
+        if oddityData["state"] == "completed":
+            subjectAllOddityData = cf.processData(oddityData)
+            allOddityData.append(subjectAllOddityData)
+            
+    # Spatial Data
+    if spatialData is 'None':
+        errors.append('No Spatial data for participant - %s' % (participantData[0]['panelId']))
+    else:        
+        # Oddity data processing
+        if spatialData["state"] == "completed":
+            subjectAllSpatialData = cf.processData(spatialData)
+            allSpatialData.append(subjectAllSpatialData)
+            
+            
+    # PAL Data
+    if palData is 'None':
+        errors.append('No PAL data for participant - %s' % (participantData[0]['panelId']))
+    else:        
+    # PAL data processing
+        if palData["state"] == "completed":
+            subjectAllPalData = cf.processData(palData)
+            allPalData.append(subjectAllPalData)
+        
+# Flatten Data
+
+finalOddity = cf.flattenData(allOddityData)
+finalSpatial = cf.flattenData(allSpatialData)
+finalPal = cf.flattenData(allPalData)
+
+# Create Output Folder
+if not os.path.exists(outputFolder):
+    os.makedirs(outputFolder)
+    
+# Output to CSV
+finalOddity.to_csv(oddityOutput)
+finalSpatial.to_csv(spatialOutput)
+finalPal.to_csv(palOutput)
+
+# Output errors
+errors = pd.DataFrame(errors)
+errors.to_csv(errorOutput)
--- CWTCHFunctions.py
+++ CWTCHFunctions.py
@@ -1,3 +1,6 @@
+## This file contains all of the functions used in the CWTCH Analysis Script (CWTCHAnalysisScript.py)
+
+#%% The loadData function is used to load the json data file.  It can contain mulitple subjects.
 def loadData( filename ):
 
     import json
@@ -7,3 +10,253 @@ def loadData( filename ):
 
     assert isinstance(jsondata, object)
     return jsondata
+
+#%% The loadParticipant function is used to extract all data for a single subject.
+def loadParticipant( alljsondata, participantid ):
+    
+    participantdata = alljsondata[participantid]
+    
+    return participantdata
+
+#%% The getTaskData function is used to extract the individual tasks from the subject's data.
+def getTaskData ( participantdata, taskid ):
+    
+    taskdata = next((item for item in participantdata if item["taskId"] == taskid), 'None')
+  
+    return taskdata
+
+#%% this function processes the data trial-by-trial and produces a flattened list to return back to the main script
+def processData (data):
+    
+    completeddata = [] # creates an empty list to store the trial data
+    
+    # the following if statement looks to see the task-type
+    if data['taskId'] == 'oddity':
+        
+        phaseData = data['phases']
+                
+        # get general data
+        participantId = data['panelId']
+        datetimeStarted = data['created']
+        
+        # this loop runs through each phase and flattens the data
+        for phaseNo, phase in enumerate(phaseData):
+    
+            kind = phase['phase']['kind']
+            condition = phase['phase']['type']
+            block = phase['blocks']
+            # this if statement is added as I wasnt sure if there was always  
+            # only one entry in the 'block' list just created. So its just an
+            # error check.
+            if len(block) == 1: 
+                phaseStartTime = block[0]['startBlock']
+                phaseEndTime = block[0]['endBlock']
+                trials = block[0]['trials']
+                # the following loop runs through and flattens the trial by 
+                # trial data and then appends it to the completeddata list.
+                for trialNo, trial in enumerate(trials):
+                        trialStartTime = trial['startTrial']
+                        trialEndTime = trial['endTrial']
+                        stimulus = trial['stimulus']
+                        clickedTime = trial['clicked']
+                        correct = trial['location'][3]
+                        # populate dictionary entry
+                        entry = {}
+                        entry['participantID'] = participantId
+                        entry['datetimeStarted'] = datetimeStarted
+                        entry['phaseNo'] = phaseNo
+                        entry['kind'] = kind
+                        entry['condition'] = condition
+                        entry['phaseStartTime'] = phaseStartTime
+                        entry['phaseEndTime'] = phaseEndTime
+                        entry['trialNo'] = trialNo
+                        entry['trialStartTime'] = trialStartTime
+                        entry['trialEndTime'] = trialEndTime
+                        entry['stimulus'] = stimulus
+                        entry['clickedTime'] = clickedTime
+                        entry['correct'] = correct
+                        #add to list
+                        completeddata.append(entry)
+            else:
+                exit("oddity length of block is greater than one - something wrong!")
+         
+    elif data['taskId'] == 'spatial':
+        
+        phaseData = data['phases']
+                
+        # get general data
+        participantId = data['panelId']
+        datetimeStarted = data['created']
+        
+        # this loop runs through each phase and flattens the data
+        for phaseNo, phase in enumerate(phaseData):
+    
+            kind = phase['phase']
+            phaseStartTime = phase['startPhase']
+            trials = phase['blocks'][0]['trials']
+            # the following loop runs through and flattens the trial by 
+            # trial data and then appends it to the completeddata list.
+            for trialNo, trial in enumerate(trials):
+                scndresp = bool(0) # this resets the boolean value that tells the loop below which response it s.
+                trialStartTime = trial['startTrial']
+                trialEndTime = trial['endTrial']
+                condition = trial['condition']
+                set1 = trial['set'][0]
+                set2 = trial['set'][1]
+                set3 = trial['set'][2]
+                stimuli = trial['stimuli']
+
+                # the following loop and if statements runs through each each 
+                # stimuli in the trial and looks to see if it is correct or not.
+                # It looks a bit complicated because it needs to check whether 
+                # the trial contains two possible targets or just one.
+                for index, stimulus in enumerate(stimuli):
+                    if set3 == 6:
+                        if scndresp is bool(0):
+                            if 'isTarget' in stimulus.keys():
+                                targetItem1 = (index)
+                                scndresp = bool(1)
+                                if 'clicked' in stimulus.keys():
+                                    response1 = bool(1)
+                                    rt1 = stimulus['clicked']
+                                else:
+                                    response1 = bool(0)
+                                    rt1 = float('nan')
+                        elif scndresp is bool(1):
+                            if 'isTarget' in stimulus.keys():
+                                targetItem2 = (index)
+                                if 'clicked' in stimulus.keys():
+                                    response2 = bool(1)
+                                    rt2 = stimulus['clicked']
+                                else:
+                                    response2 = bool(0)
+                                    rt2 = float('nan')
+                    elif set3 == 7:
+                        if 'isTarget' in stimulus.keys():
+                            targetItem1 = (index)
+                            targetItem2 = float('nan')
+                            if 'clicked' in stimulus.keys():
+                                response1 = bool(1)
+                                rt1 = stimulus['clicked']
+                                response2 = float('nan')
+                                rt2 = float('nan')
+                            else:
+                                response1 = bool(0)
+                                rt1 = float('nan')
+                                response2 = float('nan')
+                                rt2 = float('nan')
+                        
+                # populate dictionary entry
+                entry = {}
+                entry['participantID'] = participantId
+                entry['datetimeStarted'] = datetimeStarted
+                entry['phaseNo'] = phaseNo
+                entry['phase'] = kind
+                entry['condition'] = condition
+                entry['phaseStartTime'] = phaseStartTime
+                entry['trialNo'] = trialNo
+                entry['trialStartTime'] = trialStartTime
+                entry['trialEndTime'] = trialEndTime
+                entry['set1'] = set1
+                entry['set2'] = set2
+                entry['set3'] = set3
+                entry['stimulus'] = stimulus['item']
+                entry['targetItem1'] = targetItem1
+                entry['response1'] = response1
+                entry['rt1'] = rt1
+                entry['targetItem2'] = targetItem2
+                entry['response2'] = response2
+                entry['rt2'] = rt2
+                #add to list
+                completeddata.append(entry)
+        
+    
+    elif data['taskId'] == 'pal':
+        
+        phaseData = data['phases']
+                
+        # get general data
+        participantId = data['panelId']
+        datetimeStarted = data['created']
+        answer1 = data['answers'][0]
+        answer2 = data['answers'][1]
+                
+        # this loop runs through each phase and flattens the data
+        for phaseNo, phase in enumerate(phaseData):
+            
+            kind = phase['kind']
+            block = phase['blocks']
+            phaseStartTime = phase['startPhase']
+            
+            # this if statement is added as I wasnt sure if there was always  
+            # only one entry in the 'block' list just created. So its just an
+            # error check.
+            if len(block) == 1:
+                phaseStartTime = block[0]['startBlock']
+                phaseEndTime = block[0]['endBlock']
+                trials = block[0]['trials']
+                # the following loop runs through and flattens the trial by 
+                # trial data and then appends it to the completeddata list.
+                for trialNo, trial in enumerate(trials):
+                        trialStartTime = trial['startTrial']
+                        trialEndTime = trial['endTrial']
+                        corrPosition = trial['stimuli'][0] #is this correct??
+                        
+                        # the following if statement was added because some
+                        # trials did not have any response. It prevents an 
+                        # error in this case and replaces the data with NaN.
+                        if 'clicked' in trial.keys():
+                            clicked = trial['clicked'][0]
+                            rt = trial['clicked'][1]
+                            correct = corrPosition == clicked
+                        else:
+                            clicked = float('nan')
+                            rt = float('nan')
+                            correct = float('nan')
+                        type = trial['type']
+                        
+                        # This statement is added because only the probe data
+                        # has a 'kind' field.
+                        if 'kind' in trial.keys():
+                            condition = trial['kind']
+                        else:
+                            condition = float('nan')
+                        
+                        # populate dictionary entry
+                        entry = {}
+                        entry['participantID'] = participantId
+                        entry['datetimeStarted'] = datetimeStarted
+                        entry['phaseNo'] = phaseNo
+                        entry['kind'] = kind
+                        entry['condition'] = condition
+                        entry['phaseStartTime'] = phaseStartTime
+                        entry['phaseEndTime'] = phaseEndTime
+                        entry['trialNo'] = trialNo
+                        entry['trialStartTime'] = trialStartTime
+                        entry['trialEndTime'] = trialEndTime
+                        entry['corrPosition'] = corrPosition
+                        entry['rt'] = rt
+                        entry['clicked'] = clicked
+                        entry['correct'] = correct
+                        entry['type'] = type
+                        entry['condition'] = condition
+                        entry['answer1'] = answer1
+                        entry['answer2'] = answer2
+                        
+                        #add to list
+                        completeddata.append(entry)
+            else:
+                exit("oddity length of block is greater than one - something wrong!")
+           
+        
+    return completeddata
+ 
+#%% This function is used to transform and flatten the list data into a pandas dataframe
+def flattenData ( data ):
+    import pandas as pd
+    a = pd.DataFrame()
+    for participant in data:
+        a = a.append(participant)
+        
+    return a
+        
