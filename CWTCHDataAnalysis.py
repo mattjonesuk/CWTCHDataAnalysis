b7d46477a1daccfa578e5f2e307ff5305e2df44c
Rikki Lissaman <rikki.lissaman@outlook.com>
2019-08-30 13:49:36 +0100

    Remove PAL from CWTCHDataAnalysis.py
    Remove all references to the PAL task from CWTCHDataAnalysis.py.

--- CWTCHDataAnalysis.py
+++ CWTCHDataAnalysis.py
@@ -52,48 +52,6 @@ oddityGroupedDataCorrOnly=oddityTestDataCorrOnly.groupby(by=['participantID', 'c
 oddityGroupedData.to_csv("%s/oddityFinalData.csv" % (figsFolder))
 oddityGroupedDataCorrOnly.to_csv("%s/oddityCorrOnlyFinalData.csv" % (figsFolder))
 
-#%% PAL analysis
-
-palComplete = pd.DataFrame.from_csv(os.path.join(dataFolder,'palComplete.csv'))
-
-# Select only test trials (not probes)
-palTestData = palComplete[palComplete['kind'] == 'dpal'] 
-
-# Group together trials into six bins of 16 trials
-palFlattenedData = pd.DataFrame(columns=['participantID', 'bin', 'correct', 'rt'])
-for index, iParticipant in enumerate(set(palTestData['participantID'])):
-    participantPalData = palTestData[palTestData['participantID']==iParticipant]
-    participantPalBinArray = pd.DataFrame(columns=['participantID', 'bin', 'correct', 'rt'],index=range(1,7))
-    for bin in range(1,7):
-        if bin == 1:
-            binCorrect = participantPalData['correct'].iloc[range(0,16)].mean()
-            binRT = participantPalData['rt'].iloc[range(0,16)].mean()
-        elif bin == 2:
-            binCorrect = participantPalData['correct'].iloc[range(16,32)].mean()
-            binRT = participantPalData['rt'].iloc[range(16,32)].mean()  
-        elif bin == 3:
-            binCorrect = participantPalData['correct'].iloc[range(32,48)].mean()
-            binRT = participantPalData['rt'].iloc[range(32,48)].mean() 
-        elif bin == 4:
-            binCorrect = participantPalData['correct'].iloc[range(48,64)].mean()
-            binRT = participantPalData['rt'].iloc[range(48,64)].mean()
-        elif bin == 5:
-            binCorrect = participantPalData['correct'].iloc[range(64,80)].mean()
-            binRT = participantPalData['rt'].iloc[range(64,80)].mean()  
-        elif bin == 6:
-            binCorrect = participantPalData['correct'].iloc[range(80,96)].mean()
-            binRT = participantPalData['rt'].iloc[range(80,96)].mean() 
-            
-        participantPalBinArray.set_value(bin,'participantID',iParticipant)
-        participantPalBinArray.set_value(bin,'bin',bin)
-        participantPalBinArray.set_value(bin,'correct',binCorrect)
-        participantPalBinArray.set_value(bin,'rt',binRT)
-    palFlattenedData = palFlattenedData.append(participantPalBinArray, ignore_index='true')
-    palFlattenedData['bin'] = palFlattenedData['bin'].astype('category')
-
-# Export Data
-palFlattenedData.to_csv("%s/palFinalData.csv" % (figsFolder))
-
 #%% Spatial analysis
 
 spatialComplete = pd.DataFrame.from_csv(os.path.join(dataFolder,'spatialComplete.csv'))
