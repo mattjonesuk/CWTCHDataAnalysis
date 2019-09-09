27428908110f0c8ad01d2526a2a536bf932846b9
Rikki Lissaman <rikki.lissaman@outlook.com>
2019-09-09 12:18:12 +0100

    Altered error check in CWTCHDataCleaning.py
    Changed "abandoned" to "aborted" in line with changes made to JSON output.

--- CWTCHDataCleaning.py
+++ CWTCHDataCleaning.py
@@ -60,8 +60,8 @@ for index, participantData in enumerate(jsonData):
     # Oddity Data
     if oddityData is 'None':
         errors.append('No Oddity data for participant - %s' % (participantData[0]['panelId']))
-    elif oddityData["state"] == "abandoned":
-        errors.append('Oddity abandoned for participant - %s' % (participantData[0]['panelId']))
+    elif oddityData["state"] == "aborted":
+        errors.append('Oddity aborted for participant - %s' % (participantData[0]['panelId']))
     elif oddityData["state"] == "completed":
             subjectAllOddityData = cf.processData(oddityData)
             allOddityData.append(subjectAllOddityData)
@@ -69,8 +69,8 @@ for index, participantData in enumerate(jsonData):
     # Spatial Data
     if spatialData is 'None':
         errors.append('No Spatial data for participant - %s' % (participantData[0]['panelId']))
-    elif spatialData["state"] == "abandoned":
-        errors.append('Spatial abandoned for participant - %s' % (participantData[0]['panelId']))
+    elif spatialData["state"] == "aborted":
+        errors.append('Spatial aborted for participant - %s' % (participantData[0]['panelId']))
     elif spatialData["state"] == "completed":
         subjectAllSpatialData = cf.processData(spatialData)
         allSpatialData.append(subjectAllSpatialData)
